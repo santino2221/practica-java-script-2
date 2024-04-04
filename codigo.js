b9b7faa15fd72075863b6d3d4f3251b14aa7380e
santino2221 <166014288+santino2221@users.noreply.github.com>
2024-04-04 08:30:08 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,29 @@
+   
+       let numero1 = 360
+       let numero2 = 25
+       let suma = numero1 + numero2;
+       let resta = numero1 - numero2;
+       let multiplicacion = numero1 * numero2;
+       let division = numero1 / numero2;
+       let diez = 10;
+       let tres = 3;
+      console.log('La suma es ' + suma);
+       
+       console.log("La resta es " + resta);
+       console.log("La multiplicacion es " + multiplicacion)
+       console.log("La division es " + division);
+       alert ('La suma es ' + suma);
+       alert ("La resta es " + resta);
+       alert ("La multiplicacion es " + multiplicacion);
+       let resultadoMultiplicacion = diez * tres;
+       alert ("el resultado de la multiplicacion");
+       alert ("La division es " + division);
+       let cuandoNacio = prompt("Cuando naciste");
+       let edad = 2024 - cuandoNacio;
+       alert (edad);
+       let nombre = prompt("Cual es tu nombre");
+       alert ("Hola " + nombre + " bienvenido")
+       let nombre2 = prompt("Cual es tu nombre");
+       let apellido = prompt("Cual es tu apellido");
+       console.log ("Hola " + nombre2 + apellido + " bienvenido");
+       alert ("Hola" + nombre2 + apellido + " bienvenido ");
